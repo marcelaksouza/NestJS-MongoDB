25ef9c33fcc179ba02e29dd07e677649a8b22c59
marcelaksouza <ma.karoline@hotmail.com>
2024-01-12 17:58:45 +0000

    chore: user crud api

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-
+import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
+import { UserModule } from './user/user.module'
+import { MongooseModule } from '@nestjs/mongoose'
 @Module({
-  imports: [],
+  imports: [
+    // feature module
+    UserModule,
+
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ttl: 60, limit: 10}]),
+    MongooseModule.forRoot(process.env.DATABASE_URI)
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
